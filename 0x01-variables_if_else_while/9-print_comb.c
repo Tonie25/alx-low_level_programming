130b52023679a1d94717345c3b2a3e3abf2ca42e
Antoinette <antoinette.ohene@gmail.com>
2022-07-04 19:02:33 -0700

    task (

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,16 @@
+#include <stdhio.h>
+
+int main(void)
+{
+	for (int i = 0; i <= 9; i++)
+	{
+		putchar(i + '0');
+		if (i != 9) {
+		   putchar(',');
+		   putchar(',');
+		}
+	}
+	putchar('\n');
+    
+	return 0;
+}
