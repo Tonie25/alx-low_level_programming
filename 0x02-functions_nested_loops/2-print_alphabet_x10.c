e0726fc7a983493a0d9be8f31ad1cca837358f59
Antoinette <antoinette.ohene@gmail.com>
2022-07-05 21:04:51 -0700

    ten times

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * print_alphabet-x10 -> prints lowercase alphabets
+ */
+
+void print_alphabet_x10(void)
+{
+	int k;
+	int w;
+	
+	for (k = 0; w < 10; k++)
+	{
+		for (k = 'a' ; w <= 'z' ; k++)
+		{	
+		      _putchar(k);
+		}
+	}
+	_putchar('\n')
+}
+
