42a6899b85a0d0bdcfa19bffa4b60b5638a2adde
Antoinette <antoinette.ohene@gmail.com>
2022-07-06 21:04:42 -0700

    task 15

--- 0x02-functions_nested_loops/103-fibonacci.c
+++ 0x02-functions_nested_loops/103-fibonacci.c
@@ -3,7 +3,7 @@
 /**
 * mian - finds and prints the sum of the even value terms
 * followed by a new line 
-* Return Always 0 (Success0
+* Return Always 0 (Success)
 */
 int main(void)
 {
@@ -11,7 +11,7 @@ int main(void)
 	unsigned long int j, k, next, sum;
 
 	j = 1;
-	k= 2;
+	k = 2;
 	sum = 0;
 
 	for (i = 1; i <= 33; ++i)
