5a093ac343079835df028880ac60e7294163381c
Antoinette <antoinette.ohene@gmail.com>
2022-07-11 22:25:38 -0700

    task 8

--- 0x05-pointers_arrays_strings/8-print_array.c
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -1,7 +1,7 @@
 #include "main.h"
 #include <stdio.h>
 /**
- * print_array - prints n elements of an array of integers
+ *print_array - prints n elements of an array of integers
  * @a: array pointer
  * @n: number of elements
  *
