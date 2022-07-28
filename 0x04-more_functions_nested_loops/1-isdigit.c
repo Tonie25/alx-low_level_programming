c3d2f12405719e3d562ac3ec318e64e60ed5dc7e
Antoinette <antoinette.ohene@gmail.com>
2022-07-27 18:49:05 -0700

    task 1

--- 0x04-more_functions_nested_loops/1-isdigit.c
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -2,13 +2,13 @@
 /**
 *_isdigit - function that confirms whether or not a character is a digit. 
 *@c:  character to be checked
-*Return: returns 1 if it is uppercase , 0 if not
+*Return: return 1 if it is uppercase , 0 if not
 */
 
 int _isdigit(int c)
 {
 if ((c >= 48) && (c <= 57))
 return (1);
-
+else 
 return (0);
 }
